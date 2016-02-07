4bbffb3bdc2876167e76ea11c9dc0be450728717
banjocat <jackmuratore@gmail.com>
2016-02-07 10:28:35 -0500

    Solve problem 30

--- 30/main.py
+++ 30/main.py
@@ -1,5 +1,4 @@
 import sys
-from itertools import filterfalse
 
 def main():
     with open(sys.argv[1], 'r') as test_cases:
@@ -7,21 +6,24 @@ def main():
             solve(test)
 
 
-if 
-
 def solve(test):
     if test == '\n':
         return
     (first, second) = test.split(';')
     first_list = first.split(',')
     second_list = second.split(',')
-    first_set = [int(x) for x in first_list]
-    second_set = [int(x) for x in second_list]
+    first_num_list = [int(x) for x in first_list]
+    second_num_list = [int(x) for x in second_list]
 
-    intersection = sorted(first_set.intersection(second_set))
-    for x in filterfalse(lambda j: not j.isdigit() and j != ',', str(intersection)):
+    intersection = [x for x in first_num_list if x in second_num_list]
+
+    for x in str(intersection):
+        if not x.isdigit() and x != ',': 
+            continue
         sys.stdout.write(str(x))
+
     sys.stdout.write('\n')
+
 
 if __name__ == '__main__':
     main()
